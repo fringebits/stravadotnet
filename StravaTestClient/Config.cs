9210d1dde9560452b9fe430f5069380f034cb173
mikeg <mikeg@fringebits.com>
2016-02-01 22:28:15 -0800

    Added config definition.

---
+++ StravaTestClient/Config.cs
@@ -0,0 +1,26 @@
+﻿namespace StravaTestClient
+{
+    public class Config : Utilities.IConfig
+    {
+        public string Fullpath { get; set; }
+
+        public string Token { get; set; }
+
+        public string AuthCode { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string Version
+        {
+            get { return "1.0"; }
+        }
+
+        public void CreateDefaultConfig()
+        {
+            this.ClientId = "<client-id>";
+            this.ClientSecret = "<client-secret>";
+        }
+    }
+}
